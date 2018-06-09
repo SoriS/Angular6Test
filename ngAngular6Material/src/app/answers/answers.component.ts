bdf4427abc484072beae25c937af035881ee4a6d
SoriS <pashanikoluyk@mail.ru>
2018-06-09 22:00:25 +0300

    update

--- ngAngular6Material/src/app/answers/answers.component.ts
+++ ngAngular6Material/src/app/answers/answers.component.ts
@@ -1,4 +1,7 @@
+import { Questions } from './../questions/questions.component';
 import { Component, OnInit } from '@angular/core';
+import { JsonpInterceptor } from '@angular/common/http';
+import { Alert } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-answers',
@@ -8,6 +11,32 @@ import { Component, OnInit } from '@angular/core';
 export class AnswersComponent implements OnInit {
 
   constructor() { }
+
+  StartTest() {
+    const json = window.localStorage.getItem('categories');
+    if (json == null || !json.length) {
+      alert('Что то пошло не так. Пожалуйста обратитесь к системному администратору!');
+    }
+    const questions = JSON.parse(json) as Questions[];
+
+    for (let i = 0; i < questions.length; i++) {
+      const question = questions[i];
+      for (let j = 0; j < question.questions.length; j++) {
+
+        const oneQuestion = question.questions[j];
+
+        const result = confirm(oneQuestion.name);
+
+        oneQuestion.answer = result;
+
+      }
+    }
+
+    window.localStorage.setItem('categories', JSON.stringify(questions));
+
+    alert('Тест пройдено успішно!');
+
+  }
 
   ngOnInit() {
   }
--- ngAngular6Material/src/app/questions/questions.component.ts
+++ ngAngular6Material/src/app/questions/questions.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnChanges, SimpleChanges } from '@angular/core';
 import { ThrowStmt } from '@angular/compiler';
 import { MatPaginator, MatTableDataSource } from '@angular/material';
 
@@ -8,39 +8,97 @@ import { MatPaginator, MatTableDataSource } from '@angular/material';
   templateUrl: './questions.component.html',
   styleUrls: ['./questions.component.css']
 })
-export class QuestionsComponent implements OnInit {
-  displayedColumns = ['position', 'name'];
-  tableArray: Array<string> = [];
+export class QuestionsComponent implements OnInit, OnChanges {
   Row = { position: 5, name: 'sda' };
+  myData: PeriodicElement[] = [
+    { position: 1, name: 'Питання 1 ' },
+    { position: 2, name: 'Питання 2' },
+    { position: 3, name: 'Питання 3' },
+    { position: 4, name: 'Питання 4' }
+  ];
+
+  categories: Questions[] = [
+    {
+      position: 1,
+      name: 'Категория 1 ',
+      questions: <PeriodicElement[]>[
+        { position: 1, name: 'Питання 1.1 ' },
+        { position: 2, name: 'Питання 1.2' },
+        { position: 3, name: 'Питання 1.3' },
+        { position: 4, name: 'Питання 1.4' }
+      ]
+    },
+    {
+      position: 1,
+      name: 'Категория 2',
+      questions: <PeriodicElement[]>[
+        { position: 1, name: 'Питання 2.1 ' },
+        { position: 2, name: 'Питання 2.2' },
+        { position: 3, name: 'Питання 2.3' },
+        { position: 4, name: 'Питання 2.4' }
+      ]
+    },
+
+  ];
+  //   //{ position: 2, name: 'Категория 2 ' },
+  // ];
+
+  selectedCategory: Questions = this.categories[0];
+
+  SelectCategory(data: Questions): void {
+    if (data == null) { return; }
+
+    this.selectedCategory = data;
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    throw new Error('Method not implemented.');
+  }
+  RemoveItem = (data) => {
+
+    const index = this.selectedCategory.questions.indexOf(data);
+    this.selectedCategory.questions.splice(index, 1);
+    this.WriteToLocalStorage();
+  }
+  WriteToLocalStorage() {
+    const jsonData = JSON.stringify(this.categories);
+    window.localStorage.setItem('categories', jsonData);
+  }
   AddQuestion = () => {
+
+    this.Row.position = this.myData.length;
+
     this.Row.position++;
     // tslint:disable-next-line:prefer-const
-    let tempRow = Object.assign({}, this.Row);
-    this.dataSource.data.push(tempRow);
-    this.dataSource._updateChangeSubscription();
+    let tempRow = <PeriodicElement>Object.assign({}, this.Row);
+
+    if (!tempRow.name.length) {
+      return;
+
+    }
+    this.selectedCategory.questions.push(tempRow);
+    this.Row.name = '';
+    this.WriteToLocalStorage();
+  }
+  ngOnInit() {
+    const categoriesFromLocalStorage = window.localStorage.getItem('categories');
+    if (categoriesFromLocalStorage != null) {
+      this.categories = JSON.parse(categoriesFromLocalStorage) as Questions[];
+      this.selectedCategory = this.categories[0];
+    }
     this.Row.name = '';
   }
-
-  // tslint:disable-next-line:member-ordering
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-
-  // tslint:disable-next-line:member-ordering
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-
-  ngOnInit() {
-    this.dataSource.paginator = this.paginator;
-  }
-
 }
 
-export interface PeriodicElement {
+export class Questions {
+  position: number;
+  name: string;
+  questions: PeriodicElement[];
+}
+
+export class PeriodicElement {
   name: string;
   position: number;
+  answer?: boolean;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Питання 1 ' },
-  { position: 2, name: 'Питання 2' },
-  { position: 3, name: 'Питання 3' },
-  { position: 4, name: 'BeryПитання 4' }
-];
+
